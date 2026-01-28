fd8a389be4943d3951ebaf4a44efc262d6288628
Saif Ali Khan <saffellikhan@gmail.com>
2026-01-28 19:29:00 +0500

    refactor: removed drizzle and added surrealdb

--- database.ts
+++ database.ts
@@ -1,5 +1,26 @@
-import { neon } from "@neondatabase/serverless";
-import { drizzle } from "drizzle-orm/neon-http";
 import { Env } from "./core/common/env.ts";
+import { Surreal } from "surrealdb";
 
-export const db = drizzle({ client: neon(Env.getSync("DATABASE_URL")) });
+export const db = new Surreal();
+
+const connectionString = Env.getSync("DATABASE_URL");
+const connectionURL = new URL(connectionString);
+
+const namespace = connectionURL.searchParams.get("namespace") ?? undefined;
+const database = connectionURL.searchParams.get("database") ?? undefined;
+const username = connectionURL.username;
+const password = connectionURL.password;
+
+await db.connect(
+  connectionString,
+  {
+    namespace,
+    database,
+    auth: {
+      namespace,
+      database,
+      username,
+      password,
+    },
+  },
+);
